2a1cce18e72e567cca0d67c772b4407e11a4809b
ChristopherJohnAlappatt <ajchristopher2001@gmail.com>
2022-06-06 17:54:37 +0530

    initial commit

---
+++ Vectorization/example2.py
@@ -0,0 +1,28 @@
+import numpy as np
+import time as time
+
+n = 10000000
+
+a = np.random.rand(n)
+b = np.random.rand(n)
+c = []
+
+start_time = time.time()
+for i in range(n):
+    c.append(a[i] * b[i])
+
+end_time = time.time()
+# print(c)
+
+t1 = end_time - start_time
+print("Time taken using for-loop : " + str(t1 * 1000), "ms")
+
+tic = time.time()
+c = np.multiply(a,b)
+toc = time.time()
+t2 = toc - tic
+# print(c)
+print("Time taken using Vectorization : " + str(t2 * 1000), "ms")
+
+speed_up_percentage = (t1 - t2) * 100 / t1
+print("Speed up percentage", speed_up_percentage)
